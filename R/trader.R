b3b634d2c9619fe3f98a984d769c844a7289ab50
Riaz Arbi <riazarbi@gmail.com>
2022-11-01 16:56:41 +0200

    make trader more general than just alpaca

--- R/trader.R
+++ R/trader.R
@@ -1,10 +1,11 @@
 
 
-alpaca_trader <- function(orders,
+trader <- function(orders,
                           trader_life = 300,
                           resubmit_interval = 5,
                           trading_connection,
                           pricing_connection = NULL,
+                          pricing_spread_tolerance = 0.01,
                           pricing_overrides = NULL,
                           verbose = TRUE) {
   start <- Sys.time()
@@ -34,11 +35,11 @@ alpaca_trader <- function(orders,
         apply_price_limits(override_values = pricing_overrides)
     } else {
       orders_priced <- new_orders |> 
-        apply_price_limits(pricing_connection, spread_tolerance = 0.1) 
+        apply_price_limits(pricing_connection, spread_tolerance = pricing_spread_tolerance) 
     }
     
     if(verbose){message(paste0(" - submitting orders"))}
-    submitted <- alpaca_submit_orders(orders_priced, trading_connection)
+    submitted <- submit_orders(orders_priced, trading_connection)
     
     if(verbose){message(paste0(" - waiting ",resubmit_interval," seconds for orders to fill"))}
     Sys.sleep(resubmit_interval)
@@ -46,7 +47,7 @@ alpaca_trader <- function(orders,
     canceled <- FALSE
     while(!canceled) {
       if(verbose) {message(" - attempting to cancel all unfilled orders")}
-      canceled <- alpaca_cancel_orders(trading_connection)
+      canceled <- cancel_orders(trading_connection)
       if(!canceled) {
         if(verbose){message(" - cancellation failed. Retry in 5 seconds.")}
         Sys.sleep(5) 
@@ -54,22 +55,22 @@ alpaca_trader <- function(orders,
     } 
     if(verbose) {message(" - all open orders cancelled")}
     
+    
+    Sys.sleep(3) # backoff to allow cancel POST to update order statuses
+    
     if(verbose){message(paste0(" - getting order statuses"))}
-    order_status <- alpaca_get_orders(trading_connection, status = "all")
+    order_status <- get_orders(trading_connection)
     order_status <- dplyr::filter(order_status, id %in% submitted$id)
-    order_status <- dplyr::select(order_status, submitted_at, id, status, side, qty, filled_qty, filled_avg_price)
+    order_status <- dplyr::select(order_status, timestamp, id, status, filled, filled_avg_price)
     
     order_status <- dplyr::left_join(submitted, order_status, by = "id")
     order_status <- dplyr::mutate(order_status, status = ifelse(!is.na(status.y), status.y, status.x))
-    order_status <- dplyr::select(order_status, submitted_at, symbol, order, limit, value, side, qty, filled_qty, status, id)
-    order_status <- dplyr::mutate(order_status, 
-                                  submitted_at = lubridate::as_datetime(submitted_at),
-                                  qty = ifelse(side == "sell", -as.numeric(qty), as.numeric(qty)),
-                                  filled_qty = ifelse(side == "sell", -as.numeric(filled_qty), as.numeric(filled_qty)))
-    order_status <- dplyr::rename(order_status, timestamp = submitted_at, filled = filled_qty)
+    order_status <- dplyr::select(order_status, timestamp, symbol, order, limit, value,  filled, status, id)
     order_status <- dplyr::select(order_status, timestamp, symbol, order, limit, filled, status)
+    order_status <- dplyr::mutate(order_status, filled = ifelse(is.na(filled), 0, filled))
 
     if(verbose){message(paste0(" - calculating remaining order amounts"))}
+    # BUG: IF FILLED NA, KICKS OUT ORDER
     new_orders <- dplyr::mutate(order_status, order = order - filled)
     new_orders <- dplyr::mutate(new_orders, value = order * limit)
     new_orders <- dplyr::select(new_orders, symbol, order, value)
@@ -82,32 +83,34 @@ alpaca_trader <- function(orders,
     timed_out <- timeout_status > trader_life
     
   }
-  
+
+  if(verbose) {message("\n\ncommencing trader wind-down")}
   canceled <- FALSE
   while(!canceled) {
-    if(verbose) {message("Wind-down attempt to cancel all unfilled orders")}
-    canceled <- alpaca_cancel_orders(trading_connection)
+    if(verbose) {message(" - attempting to cancel all unfilled orders")}
+    canceled <- cancel_orders(trading_connection)
+    
     if(!canceled) {
       if(verbose){message(" - cancellation failed. Retry in 5 seconds.")}
       Sys.sleep(5) 
     }
   } 
-  if(verbose) {message("Wind-down cancellation success")}
+  if(verbose) {message(" - trader wind-down cancellation success")}
   
   
   if(no_orders) {
     if(verbose){
-      message(paste0("\n\nNO ORDERS TO EXECUTE. EXITING.\n\n"))
+      message(paste0("\n\nNO ORDERS TO EXECUTE. EXITING.\n"))
     }
     return(hstry)
     
   } else if(timed_out) {
     if(verbose) {
-      message("\n\nTIMEOUT REACHED. EXITING.\n\n")
+      message("\n\nTIMEOUT REACHED. EXITING.\n")
     }
     return(hstry)
   } else {
-    stop("Not sure why, but the function exited unexpectedly.")
+    stop("not sure why, but the function exited unexpectedly.")
   }
   
 }
