cd080ed03251fb33c7fedff526e579c0bb8321f7
Riaz Arbi <riazarbi@users.noreply.github.com>
2023-05-31 22:15:20 +0200

    one more try

--- R/get_cash.R
+++ R/get_cash.R
@@ -1,28 +1,27 @@
 #' Get cash balance from backend
 #'
 #' @param connection a trading backend connection
-#'
-#' @return a data frame cash balance
-#' @export
-#' @importFrom dplyr select rename mutate
-#'
-get_cash <- function(connection) {
+ #'
+ #' @return a data frame cash balance
+ #' @export
+ #' @importFrom rlang .data
+ #' @importFrom dplyr select rename mutate
+ #'
+ get_cash <- function(connection) {
 
-  # backend implementations
-  if(connection$backend == "alpaca") {
-    acct <- alpaca_account(connection)
-    acct_cash <- dplyr::select(acct, "currency", "cash")
-    acct_cash <- dplyr::rename(acct_cash, quantity_held = .data$cash)
-    acct_cash <- dplyr::mutate(acct_cash,
-                               quantity_held = as.numeric(.data$quantity_held))
+   # backend implementations
+   if(connection$backend == "alpaca") {
+     acct <- alpaca_account(connection)
+     acct_cash <- dplyr::select(acct, "currency", "cash")
+     acct_cash <- dplyr::rename(acct_cash, quantity_held = .data$cash)
+     acct_cash <- dplyr::mutate(acct_cash,
+                                quantity_held = as.numeric(.data$quantity_held))
 
-  } else {
-    stop("backend connection failed")
+   } else {
+     stop("backend connection failed")
   }
-
   # generic tests
-  test <- identical(colnames(acct_cash), c(.data$currency, .data$quantity_held))
-
+  test <- identical(colnames(acct_cash), c("currency", "quantity_held"))
   if(!test) {
     stop("data validation failed")
   } else {
