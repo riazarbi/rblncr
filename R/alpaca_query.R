0ba384c79bf2d78d56f1f450dcbdf92767e2efa0
Riaz Arbi <riazarbi@users.noreply.github.com>
2023-06-01 16:14:25 +0200

    use alpaca recommended method

--- R/alpaca_query.R
+++ R/alpaca_query.R
@@ -1,12 +1,10 @@
-alpaca_query <- function(endpoint, alpaca_connection) {
+alpaca_query <- function(endpoint, alpaca_connection){
   url <- paste0(alpaca_connection$domain, endpoint)
-  
-  result <- httr::GET(url, 
-                      alpaca_connection$headers)
-  
-  if(result$status_code == 200) {
+  result <- httr::GET(url, alpaca_connection$headers,httr::content_type("application/octet-stream"), httr::accept("application/json"))
+  if (result$status_code == 200) {
     return(httr::content(result))
-  } else {
-    stop(result$status_code)    
+  }
+  else {
+    stop(result$status_code)
   }
 }
