eb9d06ad12677b63757656b8bb122ff543ba0b66
Riaz Arbi <riazarbi@gmail.com>
2024-07-23 13:44:31 +0200

    add config parameter

--- R/make_http_get_request.R
+++ R/make_http_get_request.R
@@ -1,3 +1,3 @@
-make_http_get_request <- function(url, headers, content_type, accept) {
-  httr::GET(url, headers, content_type, accept)
+make_http_get_request <- function(url, headers, content_type, accept, config) {
+  httr::GET(url, headers, content_type, accept, config)
 }
