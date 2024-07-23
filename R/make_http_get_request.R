e3c3ea3d6d87ed450263c9657915df8dc5069f06
Riaz Arbi <riazarbi@gmail.com>
2024-07-23 14:03:11 +0200

    ad default arg for httr::config

--- R/make_http_get_request.R
+++ R/make_http_get_request.R
@@ -1,3 +1,3 @@
-make_http_get_request <- function(url, headers, content_type, accept, config) {
+make_http_get_request <- function(url, headers, content_type, accept, config = httr::config()) {
   httr::GET(url, headers, content_type, accept, config)
 }
