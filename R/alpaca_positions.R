d2d0411e1b66c649f4649bde38e01afbf854e7dd
Riaz Arbi <riazarbi@gmail.com>
2024-07-23 13:44:16 +0200

    add missing return()

--- R/alpaca_positions.R
+++ R/alpaca_positions.R
@@ -9,4 +9,5 @@ alpaca_positions <- function(alpaca_connection) {
   positions <- dplyr::rename(positions, quantity_held = "qty")
   positions <- dplyr::mutate(positions, quantity_held = as.numeric(.data$quantity_held))
   positions <- validate_positions_class(positions)
+  return(positions)
 }
